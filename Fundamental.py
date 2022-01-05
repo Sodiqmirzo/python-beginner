874ce7b9fa7b06d6ae69f8d69aa09db5fe050802
developer <mr.sodiqmirzosattorov17@gmail.com>
2022-01-05 17:08:43 +0500

    list array

--- Fundamental.py
+++ Fundamental.py
@@ -169,14 +169,14 @@
 # print("Start")
 # print(multiply(1, 2, 3))
 
-def fizz_buzz(input):
-    if input % 3 == 0:
-        return "Fizz"
-    if input % 5 == 0:
-        return "Buzz"
-    if (input % 3 == 0) and (input % 5 == 0):
-        return "FizzBuzz"
-    return input
-
-
-print(fizz_buzz(2))
+# def fizz_buzz(input):
+#     if (input % 3 == 0) and (input % 5 == 0):
+#         return "FizzBuzz"
+#     if input % 3 == 0:
+#         return "Fizz"
+#     if input % 5 == 0:
+#         return "Buzz"
+#     return input
+#
+#
+# print(fizz_buzz(5))
